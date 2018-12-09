eccfa503df1ab41a972bc9738ec2183dce6ee1e6
kls602 <stephen.cirelli@capitalone.com>
2018-12-09 15:09:22 -0500

    v1

--- problem_7/main.py
+++ problem_7/main.py
@@ -13,7 +13,7 @@ Examples:
 [1,1,1], [11,1], [1,11]
 
 {1111} = 4
-[1,1,1,1], [11,1,1], [11,11], [1,11,1]
+[1,1,1,1], [11,1,1], [11,11], [1,11,1], [1,1,11]
 
 {1234} = 3
 [1,2,3,4], [12,3,4], [1,23,4]
@@ -23,30 +23,63 @@ Examples:
 
 {11111}
 [1,1,1,1,1],
-    [11,1,1,1],
-        [11,11,1],
-        [11,1,11],
-    [1,1,11,1],
-        [1,1,1,11],
-    [1,11,1,1],
-        [1,11,11],
-    [1,1,1,11]
+[1,11,1,1],
+[1,11,11],
 
+[1,1,11,1],
+
+[1,1,1,11],
+
+[11,1,1,1],
+[11,11,1],
+[11,1,11]
 '''
+import sys;
 
-
+cnt = 0
 def perms(input_str, start, end):
     "count message permutations"
-    cnt = 0
-    for i in range(start, end):
-        print('start', start, ' i', i)
-        if int(input_str[i:i + 2]) <= 26:
-            perms(input_str, start + 2, end)
-            cnt += 1
-    return cnt
+    global cnt
+    cnt += 1
+    for i in range(start, end-1):
+        num = input_str[i:i + 2]
+        if int(num) <= 26:
+            perms(input_str, i + 2, end)
 
 
+S = sys.stdin.read().rstrip() or '111'
+print(f"Input: '{S}'")
 breakpoint()
+perms(S, 0, len(S))
+print(cnt)
 
-S = '11111'
-print(perms(S, 0, len(S)))
+
+# ------ Stack 1 -------                                        ------ Stack 1' -------
+# cnt = 1                                                       cnt = 3
+# def perms(input_str='111', start=0, end=3):                   def perms(input_str='111', start=0, end=3):
+#     global cnt                                                    global cnt
+#     cnt += 1                    \i/                               cnt += 1                      \i/
+#     for i in range(start, end): [0,1,2]                           for i in range(start, end): [0,1,2]
+#         num = input_str[i:i + 2] '11'                                 num = input_str[i:i + 2] '11'
+#         if int(num) <= 26:                                            if int(num) <= 26:
+#             perms(input_str ='11,1', i + 2, end)                      perms(input_str ='1,11', i + 2, end)
+
+# ------ Stack 2 -------                                        ------ Stack 2' -------
+# cnt = 2                                                       cnt = 3
+# def perms(input_str='11,1', start=2, end=3):                  def perms(input_str='1,11', start=1, end=3):
+#     global cnt                                                    global cnt
+#     cnt += 1                    \i/                               cnt += 1                      \i/
+#     for i in range(start, end): [2]                               for i in range(start, end): [0,1,2]
+#         num = input_str[i:i + 2] '1'                                  num = input_str[i:i + 2] '11'
+#         if int(num) <= 26:                                            if int(num) <= 26:
+#             perms(input_str ='11,1,', i + 2, end)                     perms(input_str ='1,11', i + 2, end)
+
+# ------ Stack 3 -------
+# cnt = 3
+# def perms(input_str='11,1,', start=4, end=3):
+#     global cnt
+#     cnt += 1                    \i/
+#     for i in range(start, end): []
+#         num = input_str[i:i + 2] '1'
+#         if int(num) <= 26:
+#             perms(input_str ='11,1,', i + 2, end)
