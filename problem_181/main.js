4a677206386fe218c18995636e32fe975198dc0e
kls602 <stephen.cirelli@capitalone.com>
2019-06-16 21:34:11 -0400

    forgot to update main

--- problem_181/main.js
+++ problem_181/main.js
@@ -22,14 +22,7 @@ function readLine() {
 /////////////// ignore above this line ////////////////////
 const listFewestPalindromes = require('./listFewestPalindromes');
 
-function listFewestPalindromes(arr) {
-    for(let i=arr.length-1; i>=0; i--) {
-    }
-}
-
 // od -vAd -N40 -tu1 < /dev/urandom | cut -c11- | tr -s [:space:] " " | tee input2.txt
 function main() {
-    var arr = readLine().split('');
-    
-    console.log(listFewestPalindromes(arr));
+    console.log(listFewestPalindromes(readLine()));
 }
