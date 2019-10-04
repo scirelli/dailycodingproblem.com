d7b8ee284e1fe916fb881c3e4dcd9bc7d297b472
Steve Cirelli <scirelli+git@gmail.com>
2019-10-03 23:33:47 -0400

    clean up

--- problem_291/main.py
+++ problem_291/main.py
@@ -5,8 +5,6 @@ This problem was asked by Glassdoor.
 An imminent hurricane threatens the coastal town of Codeville. If at most two people can fit in a rescue boat, and the maximum weight limit for a given boat is k, determine how many boats will be needed to save everyone.
 
 For example, given a population with weights [100, 200, 150, 80] and a boat limit of 200, the smallest number of boats required will be three.
-
-del fpl[i:i+1]
 """
 ERASED = 0
 
@@ -46,9 +44,9 @@ def main():
     import random
 
     MAX_WEIGHT = 201
-    MIN_SIZE = 10
-    MAX_SIZE = 1001
-    fatPeopleList = [random.randrange(1, MAX_WEIGHT) for x in range(random.randrange(MIN_SIZE, MAX_SIZE))]
+    MIN_NUMBER_OF_PEOPLE = 10
+    MAX_NUMBER_OF_PEOPLE = 1001
+    fatPeopleList = [random.randrange(1, MAX_WEIGHT) for x in range(random.randrange(MIN_NUMBER_OF_PEOPLE, MAX_NUMBER_OF_PEOPLE))]
     print('Smallest number of boats for [{}] is {}'.format(', '.join(str(x) for x in fatPeopleList), findSmallestNumberOfBoats(fatPeopleList, 200)))
 
 
