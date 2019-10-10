883e03932f45822412b47f15a1629e9bbcc22a84
kls602 <stephen.cirelli@capitalone.com>
2019-10-10 19:03:20 -0400

    updates

---
+++ problem_296/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Given a sorted array, convert it into a height-balanced (AVL) binary search tree.
+
+Reference:
+    http://www.cs.ecu.edu/karl/3300/spr16/Notes/DataStructure/Tree/balance.html
+"""
