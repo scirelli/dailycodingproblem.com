d406537f46d718a870237465b172be78f4a72937
Steve Cirelli <scirelli@gmail.com>
2019-10-20 10:36:26 -0400

    first part done.

--- problem_307/main.py
+++ problem_307/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-from random import shuffle
-
 from binarySearchTree.BinarySearchTree import BinarySearchTree
+
 
 def ceiling(treeRoot, n):
     """
@@ -10,26 +9,34 @@ def ceiling(treeRoot, n):
             2
           1   3
     """
-    if not treeRoot:
+    result = None
+
+    def travers(treeRoot, n):
+        nonlocal result
+        if not treeRoot:
+            return None
+
+        travers(treeRoot.left, n)
+        travers(treeRoot.right, n)
+        if result is None and treeRoot.value >= n:
+            result = treeRoot.value
+
         return None
 
-    ceiling(treeRoot.left, n)
-    ceiling(treeRoot.right, n)
-    if treeRoot.value >= n:
-        return treeRoot.value
+    travers(treeRoot, n)
+    return result
 
-    return None
 
 def floor(treeRoot, n):
     """
     Floor is the highest element in the tree less than or equal to an integer.
     Breath first search. node <= n
     """
-    pass
 
-def test():
+
+def run_tests():
     ceilingTests = [
-        {'array': [2,1,3], 'num': 2, 'expected': 3}
+        {'array': [2, 1, 3], 'num': 2.3, 'expected': 3}
     ]
 
     for test in ceilingTests:
@@ -37,13 +44,14 @@ def test():
         t.insert(test['array'])
         result = ceiling(t.get_root(), test['num'])
         try:
-            assert(result == test['expected'])
+            assert result == test['expected']
         except AssertionError:
             print('FAIL: ', result, '!=', test['expected'])
 
+
 def main():
-    test()
+    run_tests()
 
 
 if __name__ == '__main__':
-  main()
+    main()
