ea4f9d1ed67abacc8257e3f9f544a30a90f95657
Steve Cirelli <scirelli+git@gmail.com>
2019-10-20 18:53:59 -0400

    first complete answer

--- problem_307/main.py
+++ problem_307/main.py
@@ -6,8 +6,6 @@ def ceiling(treeRoot, n):
     """
     Ceiling is the lowest element in the tree greater than or equal to an integer.
     Depth first search. node >= n
-            2
-          1   3
     """
     result = None
 
@@ -32,21 +30,45 @@ def floor(treeRoot, n):
     Floor is the highest element in the tree less than or equal to an integer.
     Breath first search. node <= n
     """
+    queue = [treeRoot]
+
+    while queue:
+        node = queue.pop(0)
+
+        if node.value <= n:
+            return node.value
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+    return None
+
+
+def runTest(testFuncName, test):
+    t = BinarySearchTree()
+    t.insert(test['array'])
+    result = globals()[testFuncName](t.get_root(), test['num'])
+    try:
+        assert result == test['expected']
+    except AssertionError:
+        print(testFuncName, 'FAIL: ', result, '!=', test['expected'])
 
 
 def run_tests():
-    ceilingTests = [
-        {'array': [2, 1, 3], 'num': 2.3, 'expected': 3}
-    ]
+    tests = {
+        'floor': [
+            {'array': [2, 1, 3], 'num': 2.3, 'expected': 2}
+        ],
+        'ceiling': [
+            {'array': [2, 1, 3], 'num': 2.3, 'expected': 3}
+        ]
+    }
 
-    for test in ceilingTests:
-        t = BinarySearchTree()
-        t.insert(test['array'])
-        result = ceiling(t.get_root(), test['num'])
-        try:
-            assert result == test['expected']
-        except AssertionError:
-            print('FAIL: ', result, '!=', test['expected'])
+    for testFuncName, tests in tests.items():
+        for test in tests:
+            runTest(testFuncName, test)
 
 
 def main():
