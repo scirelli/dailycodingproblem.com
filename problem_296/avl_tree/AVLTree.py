8dd0c244efc54f08f2d2c460be07357db9e0c991
Steve Cirelli <scirelli@gmail.com>
2019-10-29 21:02:55 -0400

    updates

--- problem_296/avl_tree/AVLTree.py
+++ problem_296/avl_tree/AVLTree.py
@@ -49,3 +49,141 @@ class AVLTree():
         node = right_tree
         AVLTree.calculate_height(node)
         return node
+
+    @staticmethod
+    def single_rotation_right(node):
+        r"""
+
+            Tree L   = T->left;
+            T->left  = L->right;
+            installHeight(T);
+
+            L->right = T;
+            T        = L;
+            installHeight(T);
+        """
+        left_tree = node.left_child
+        node.left_child = left_tree.right_child
+        AVLTree.calculate_height(node)
+
+        left_tree.right_child = node
+        node = left_tree
+        AVLTree.calculate_height(node)
+
+        return node
+
+    @staticmethod
+    def double_rotation_left(node):
+        """
+        double_rotate_left(T) performs a double rotation from right to left at the root of T.
+
+        single_rotation_right(T.right_child);
+        single_rotation_left(T);
+        """
+        node.right_child = AVLTree.single_rotation_right(node.right_child)
+        return AVLTree.single_rotation_left(node)
+
+    @staticmethod
+    def double_rotation_right(node):
+        """
+        doubleRotateRight(T) performs a double rotation from left to right at the root of T.
+
+        single_rotation_left(T->left);
+        single_rotation_right(T);
+        """
+        node.left_child = AVLTree.single_rotation_left(node.left_child)
+        return AVLTree.single_rotation_right(node)
+
+    @staticmethod
+    def rotate_left(node):
+        """
+        rotate_left(T) performs a rotation from right to left at the root of T, choosing a single or double rotation.
+
+        Tree r   = T->right;
+        int  zag = height(r->left);
+        int  zig = height(r->right);
+
+        if(zig > zag)
+        {
+            singleRotateLeft(T);
+        }
+        else
+        {
+            doubleRotateLeft(T);
+        }
+        """
+        right_tree = node.right_child
+        zag = AVLTree.height(right_tree.left_child)
+        zig = AVLTree.height(right_tree.right_child)
+
+        if zig > zag:
+            node = single_rotation_left(node)
+        else:
+            node = double_rotation_left(node)
+
+        return node
+
+    @staticmethod
+    def rotate_right(node):
+        """
+        rotateRight(T) performs a rotation from left to right at the root of T, choosing a single or double rotation.
+
+        Tree L  = T->left;
+        int  zig = height(L->left);
+        int  zag = height(L->right);
+
+        if(zig > zag)
+        {
+        singleRotateRight(T);
+        }
+        else
+        {
+        doubleRotateRight(T);
+        }
+        """
+        left_tree = node.left_child
+        zig = AVLTree.height(left_tree.left_child)
+        zag = AVLTree.height(left_tree.right_child)
+
+        if zig > zag:
+            node = single_rotation_right(node)
+        else:
+            node = double_rotation_right(node)
+
+        return node
+
+    @staticmethod
+    def rebalance(node):
+        """
+        (1) Perform a rotation at T if required.
+        (2) Set the ht field of T correctly, regardless of whether or not a rotation is done.
+
+        Requirement: T must not be empty.
+
+        int hl = height(T->left);
+        int hr = height(T->right);
+
+        if(hr > hl + 1)
+        {
+            rotateLeft(T);
+        }
+        else if(hl > hr + 1)
+        {
+            rotateRight(T);
+        }
+        else
+        {
+            installHeight(T);
+        }
+        """
+        left_child_height = AVLTree.height(node.left_child)
+        right_child_height = AVLTree.height(node.right_child)
+
+        if right_child_height > left_child_height + 1:
+            node = rotate_left(node)
+        elif left_child_height > right_child_height + 1:
+            node = rotate_right(node)
+        else:
+            node.height = AVLTree.calculate_height(node)
+
+        return node
