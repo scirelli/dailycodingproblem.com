24994af4820c1edc6a37ea580a9a8ac4a414332a
scirelli <scirelli+git@gmail.com>
2020-01-11 15:02:10 -0500

    Finished 296

--- problem_296/avl_tree/AVLTree.py
+++ problem_296/avl_tree/AVLTree.py
@@ -6,8 +6,11 @@ class AVLTree():
             self.right_child = right_child
             self.height = AVLTree.calculate_height(self)
 
-    def __init__(self, root):
-        self.root = root
+        def __str__(self):
+            return str(self.value)
+
+        def __repr__(self):
+            return str(self.value)
 
     @staticmethod
     def height(node):
@@ -18,6 +21,9 @@ class AVLTree():
 
     @staticmethod
     def calculate_height(node):
+        if node is None:
+            raise ValueError('calculate_height parameter one must be a node')
+
         lh = node.left_child.height if node.left_child else 0
         rh = node.right_child.height if node.right_child else 0
         return 1 + max(lh, rh)
@@ -41,11 +47,181 @@ class AVLTree():
             T        = r;
             calculate_height(T)
         """
+        if node is None:
+            raise ValueError('single_rotate_left parameter one must be a node')
+
         right_tree = node.right_child
         node.right_child = right_tree.left_child
-        AVLTree.calculate_height(node)
+        node.height = AVLTree.calculate_height(node)
 
         right_tree.left_child = node
         node = right_tree
-        AVLTree.calculate_height(node)
+        node.height = AVLTree.calculate_height(node)
         return node
+
+    @staticmethod
+    def single_rotation_right(node):
+        r"""
+              x                y
+             / \              / \
+            /   \            /   \
+           y     C    ==>   A     x
+          / \                    / \
+         /   \                  /   \
+        A     B                B     C
+
+        Tree L   = T->left;
+        T->left  = L->right;
+        calculate_height(T);
+
+        L->right = T;
+        T        = L;
+        calculate_height(T);
+        """
+        if node is None:
+            raise ValueError('single_rotate_right parameter one must be a node')
+
+        left_tree = node.left_child
+        node.left_child = left_tree.right_child
+        node.height = AVLTree.calculate_height(node)
+
+        left_tree.right_child = node
+        node = left_tree
+        node.height = AVLTree.calculate_height(node)
+        return node
+
+    @staticmethod
+    def double_rotation_left(node):
+        if node is None:
+            raise ValueError('double_rotate_left parameter one must be a node')
+
+        node.right_child = AVLTree.single_rotation_right(node.right_child)
+        return AVLTree.single_rotation_left(node)
+
+    @staticmethod
+    def double_rotation_right(node):
+        if node is None:
+            raise ValueError('double_rotate_right parameter one must be a node')
+
+        node.left_child = AVLTree.single_rotation_left(node.left_child)
+        return AVLTree.single_rotation_right(node)
+
+    @staticmethod
+    def rotate_left(node):
+        if node is None:
+            raise ValueError('rotate_left parameter one must be a node')
+
+        right_child = node.right_child
+        left_child_height = AVLTree.height(right_child.left_child)
+        right_child_height = AVLTree.height(right_child.right_child)
+
+        if right_child_height > left_child_height:
+            return AVLTree.single_rotation_left(node)
+
+        return AVLTree.double_rotation_left(node)
+
+    @staticmethod
+    def rotate_right(node):
+        if node is None:
+            raise ValueError('rotate_right parameter one must be a node')
+
+        left_child = node.left_child
+        left_child_height = AVLTree.height(left_child.left_child)
+        right_child_height = AVLTree.height(left_child.right_child)
+
+        if left_child_height > right_child_height:
+            return AVLTree.single_rotation_right(node)
+
+        return AVLTree.double_rotation_right(node)
+
+    @staticmethod
+    def rebalance(node):
+        if node is None:
+            raise ValueError('rebalance parameter one must be a node')
+
+        left_child_height = AVLTree.height(node.left_child)
+        right_child_height = AVLTree.height(node.right_child)
+
+        if right_child_height > left_child_height + 1:
+            return AVLTree.rotate_left(node)
+
+        if left_child_height > right_child_height + 1:
+            return AVLTree.rotate_right(node)
+
+        node.height = AVLTree.calculate_height(node)
+        return node
+
+    @staticmethod
+    def insert(value, node):
+        if node is None:
+            node = AVLTree.Node(value)
+        elif value < node.value:
+            node.left_child = AVLTree.insert(value, node.left_child)
+        elif value > node.value:
+            node.right_child = AVLTree.insert(value, node.right_child)
+
+        node = AVLTree.rebalance(node)
+
+        return node
+
+    @staticmethod
+    def insertAll(values, node=None):
+        for value in values:
+            node = AVLTree.insert(value, node)
+
+        return node
+
+    @staticmethod
+    def remove_smallest(node):
+        if node is None:
+            raise ValueError('remove_smallest parameter one must be a node.')
+
+        if node.left_child is None:
+            value = node.value
+            node = node.right_child
+            result = {'value': value, 'node': node}
+        else:
+            result = AVLTree.remove_smallest(node.left_child)
+            AVLTree.rebalance(result['node'])
+
+        return result
+
+    @staticmethod
+    def remove(value, node):
+        if node:
+            if value < node.value:
+                node = AVLTree.rebalance(AVLTree.remove(value, node.left_child))
+            elif value > node.value:
+                node = AVLTree.rebalance(AVLTree.remove(value, node.right_child))
+            elif node.left_child is None:
+                node = node.right_child
+            elif node.right_child is None:
+                node = node.left_child
+            else:
+                result = AVLTree.remove_smallest(node.right_child)
+                node.value = result['value']
+                node = AVLTree.rebalance(node)
+
+        return node
+
+    @staticmethod
+    def in_order(node, buff: list = None):
+        if node is None:
+            return buff
+
+        AVLTree.in_order(node.left_child, buff)
+        buff.append(node.value)
+        AVLTree.in_order(node.right_child, buff)
+
+        return buff
+
+    @staticmethod
+    def post_order(node, buff: list = None):
+        if node is None:
+            return buff
+
+        AVLTree.post_order(node.right_child, buff)
+        buff.append(node.value)
+        AVLTree.post_order(node.left_child, buff)
+
+        return buff
--- problem_296/main.py
+++ problem_296/main.py
@@ -8,3 +8,18 @@ Reference:
     https://github.com/scirelli/jsdatastructures/tree/master/avlTree
     https://github.com/scirelli/pi_asm_armv6/tree/master/avlTree
 """
+from random import shuffle
+from avl_tree.AVLTree import AVLTree
+
+
+def main():
+    values = list(range(100))
+    # shuffle(values)
+    node = AVLTree.insertAll(values)
+    print(', '.join([ str(x) for x in AVLTree.in_order(node, [])]))
+    print('')
+    print(', '.join([str(x) for x in AVLTree.post_order(node, [])]))
+
+
+if __name__ == '__main__':
+    main()
